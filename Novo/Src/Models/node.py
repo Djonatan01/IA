54e8d9b7161884b72bf569d09ca10b922b14a987
Diego Jonatan de Miranda <103201121+Djonatan01@users.noreply.github.com>
2024-03-29 08:38:29 -0300

    Ajuste de diretorios

--- Novo/Src/Models/node.py
+++
@@ -1,8 +0,0 @@
-class No(object):
-    def __init__(self, pai=None, estado=None, nivel=None, 
-                 anterior=None,  proximo=None):
-        self.pai       = pai
-        self.estado    = estado
-        self.nivel     = nivel
-        self.anterior  = anterior
-        self.proximo   = proximo
